3c50af0e7d5ae9d4b8adabb32eb05daf4a3f7cdb
Miguel Imperial <migimperial@yahoo.com>
2023-04-29 14:37:04 +0800

    Use ModelSerializer; add README

---
+++ README.md
@@ -0,0 +1,9 @@
+### API for Philippine Native Trees (PNT)
+A database for PNTs to
+* centralize common information  about our beloved trees
+* help other developers/PNT enthusiasts build related projects
+* help others to know what native trees to plant based on their province based on "Philippine Native Trees – What to Plant in Different Provinces"
+
+![Page 1](files/images/wtp_1.jpg)
+
+![Page 33](files/images/wtp_2.jpg)
--- trees_api/ntapi/serializers.py
+++ trees_api/ntapi/serializers.py
@@ -1,25 +1,16 @@
 from rest_framework import serializers
 from ntapi.models import Species, ECOLOGICAL_CLASSIFICATION, CONSERVATION_STATUS
 
-class SpeciesSerializer(serializers.Serializer):
-    # model = Species
-    # fields = ['common_name', 'species']
-    id                  = serializers.IntegerField(read_only=True)
-    common_name         = serializers.CharField()
-    species             = serializers.CharField()
-    # family              = serializers.CharField()
-    # common_name_other   = serializers.CharField()
-    # min_altitude        = serializers.IntegerField() 
-    # max_altitude        = serializers.IntegerField()
-    # ecological_classification   = serializers.CharField()
-    # conservation_status         = serializers.CharField()
-    # alay_page           = serializers.IntegerField()
+class SpeciesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Species
+        fields = ['common_name', 'species']
 
-    def create(self, validated_data):
-        return Species.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.common_name    = validated_data.get('common_name', instance.title)
-        instance.species        = validated_data.get('species', instance.title)
-        instance.save()
-        return instance
+        # Other fields to include soon
+        # family              = serializers.CharField()
+        # common_name_other   = serializers.CharField()
+        # min_altitude        = serializers.IntegerField() 
+        # max_altitude        = serializers.IntegerField()
+        # ecological_classification   = serializers.CharField()
+        # conservation_status         = serializers.CharField()
+        # alay_page           = serializers.IntegerField()

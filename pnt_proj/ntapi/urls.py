a397ab50320624a0eef04e4c1678ad6214e2e66a
Miguel Imperial <migimperial@yahoo.com>
2023-05-29 21:13:24 +0800

    Enable search via common_name

--- pnt_proj/ntapi/urls.py
+++ pnt_proj/ntapi/urls.py
@@ -4,7 +4,8 @@ from ntapi import views
 
 urlpatterns = [
     path('ntapi/', views.SpeciesList.as_view()),
-    path('ntapi/<str:common_name>/', views.SpeciesDetail.as_view()),
+    path('ntapi/<int:pk>/', views.SpeciesDetail.as_view()),
+    path('ntapi/<str:common_name>/', views.SpeciesDetail_name.as_view(), name='common_name'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- pnt_proj/ntapi/views.py
+++ pnt_proj/ntapi/views.py
@@ -1,49 +1,17 @@
 from ntapi.models import Species
 from ntapi.serializers import SpeciesSerializer
+from rest_framework import generics
 
-from django.http import Http404
+class SpeciesList(generics.ListCreateAPIView):
+    queryset = Species.objects.all()
+    serializer_class = SpeciesSerializer
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+class SpeciesDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Species.objects.all()
+    serializer_class = SpeciesSerializer
 
-class SpeciesList(APIView):
-    # List all species, or add a new species
-
-    def get(request, format=None):
-        species = Species.objects.all()
-        serializer = SpeciesSerializer(species, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request, format=None):
-        serializer = SpeciesSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class SpeciesDetail(APIView):
-        # Retreive, update, or delete a species
-    def get_object(self, common_name):
-        try:
-            return Species.objects.get(common_name=common_name)
-        except Species.DoesNotExist:
-            raise Http404
-
-    def get(self, request, common_name, format=None):    
-        species = self.get_object(common_name)
-        serializer = SpeciesSerializer(species)
-        return Response(serializer.data)
-     
-    def put(self, request, common_name, format=None):    
-        species = self.get_object(common_name)
-        serializer = SpeciesSerializer(species)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, common_name, format=None):
-        species = self.get_object(common_name)
-        species.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class SpeciesDetail_name(generics.RetrieveUpdateDestroyAPIView):
+    # queryset = Species.objects.filter(common_name='common_name')
+    queryset = Species.objects.all()
+    lookup_field = 'common_name'
+    serializer_class = SpeciesSerializer

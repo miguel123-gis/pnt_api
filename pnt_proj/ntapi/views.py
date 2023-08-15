f3a0ac39447930e330cbab37fab5479a1b54b219
Miguel Imperial <migimperial@yahoo.com>
2023-08-15 21:14:33 +0800

    Add a view for each column

--- pnt_proj/ntapi/views.py
+++ pnt_proj/ntapi/views.py
@@ -11,7 +11,51 @@ class SpeciesDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SpeciesSerializer
 
 class SpeciesDetail_name(generics.RetrieveUpdateDestroyAPIView):
-    # queryset = Species.objects.filter(common_name='common_name')
     queryset = Species.objects.all()
     lookup_field = 'common_name'
     serializer_class = SpeciesSerializer
+
+class SpeciesDetail_species(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Species.objects.all()
+    lookup_field = 'species'
+    serializer_class = SpeciesSerializer
+
+class SpeciesDetail_family(generics.ListAPIView):
+    serializer_class = SpeciesSerializer
+
+    def get_queryset(self):
+        queryset = Species.objects.all()
+        family = self.kwargs['family']
+        if family is not None:
+            queryset = queryset.filter(family=family)
+        return queryset
+
+class SpeciesDetail_classification(generics.ListAPIView):
+    serializer_class = SpeciesSerializer
+
+    def get_queryset(self):
+        queryset = Species.objects.all()
+        eco_class = self.kwargs['ecological_classification']
+        if eco_class is not None:
+            queryset = queryset.filter(ecological_classification=eco_class)
+        return queryset
+
+class SpeciesDetail_status(generics.ListAPIView):
+    serializer_class = SpeciesSerializer
+
+    def get_queryset(self):
+        queryset = Species.objects.all()
+        con_status = self.kwargs['conservation_status']
+        if con_status is not None:
+            queryset = queryset.filter(conservation_status=con_status)
+        return queryset
+
+class SpeciesDetail_binhi(generics.ListAPIView):
+    serializer_class = SpeciesSerializer
+
+    def get_queryset(self):
+        queryset = Species.objects.all()
+        binhi_prio = self.kwargs['binhi_priority']
+        if binhi_prio is not None:
+            queryset = queryset.filter(binhi_priority=binhi_prio)
+        return queryset

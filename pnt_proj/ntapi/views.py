a134d1e186e50bd861fe978ce4cd3dc0e8afce6f
Miguel Imperial <migimperial@yahoo.com>
2024-11-25 21:00:14 +0800

    Make lookup string case-insensitive
    - Change all views to only ListAPI

--- pnt_proj/ntapi/views.py
+++ pnt_proj/ntapi/views.py
@@ -2,23 +2,33 @@ from ntapi.models import Species
 from ntapi.serializers import SpeciesSerializer
 from rest_framework import generics
 
-class SpeciesList(generics.ListCreateAPIView):
+class SpeciesList(generics.ListAPIView):
     queryset = Species.objects.all().order_by('id')
     serializer_class = SpeciesSerializer
 
-class SpeciesDetail(generics.RetrieveUpdateDestroyAPIView):
+class SpeciesDetail(generics.ListAPIView):
     queryset = Species.objects.all()
     serializer_class = SpeciesSerializer
 
-class SpeciesDetail_name(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Species.objects.all()
-    lookup_field = 'common_name'
+class SpeciesDetail_name(generics.ListAPIView):
+    serializer_class = SpeciesSerializer    
+
+    def get_queryset(self):
+        queryset = Species.objects.filter()
+        common_name = self.kwargs['common_name']
+        if common_name is not None:
+            queryset = queryset.filter(common_name__iexact=common_name)
+        return queryset
+
+class SpeciesDetail_species(generics.ListAPIView):
     serializer_class = SpeciesSerializer
 
-class SpeciesDetail_species(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Species.objects.all()
-    lookup_field = 'species'
-    serializer_class = SpeciesSerializer
+    def get_queryset(self):
+        queryset = Species.objects.filter()
+        species = self.kwargs['species']
+        if species is not None:
+            queryset = queryset.filter(species__iexact=species)
+        return queryset
 
 class SpeciesDetail_family(generics.ListAPIView):
     serializer_class = SpeciesSerializer
@@ -27,7 +37,7 @@ class SpeciesDetail_family(generics.ListAPIView):
         queryset = Species.objects.all()
         family = self.kwargs['family']
         if family is not None:
-            queryset = queryset.filter(family=family)
+            queryset = queryset.filter(family__iexact=family)
         return queryset
 
 class SpeciesDetail_classification(generics.ListAPIView):

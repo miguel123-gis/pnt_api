71c5d09d8571c12fb25dfa046b8bd6cdd5200821
Miguel Imperial <migimperial@yahoo.com>
2025-01-28 17:32:00 +0800

    Untrack gunicorn and nginx config files and logs, add example files

--- pnt_proj/gunicorn.conf.py
+++
@@ -1,5 +0,0 @@
-bind = "127.0.0.1:8000"
-errorlog = '/Users/miguel/Documents/native_trees/pnt_proj/logs/gunicorn-error.log'
-accesslog = '/Users/miguel/Documents/native_trees/pnt_proj/logs/gunicorn-access.log'
-loglevel = 'debug'
-workers = 1

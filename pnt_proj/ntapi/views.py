dd0e54cb45cdf91de6819a4bf76b910efcf28948
Miguel Imperial <migimperial@yahoo.com>
2025-02-17 21:09:22 +0800

    Allow multifield filter

--- pnt_proj/ntapi/views.py
+++ pnt_proj/ntapi/views.py
@@ -1,8 +1,12 @@
+from functools import reduce
+import operator
 from ntapi.models import Species
 from ntapi.serializers import SpeciesSerializer
 from rest_framework import generics
-
 from rest_framework.response import Response
+from django.db.models import Q
+from django.http import JsonResponse
+
 
 field_mappings = SpeciesSerializer.field_mappings
 
@@ -11,30 +15,49 @@ class SpeciesList(generics.ListAPIView):
     serializer_class = SpeciesSerializer
 
 class SpeciesDetail(generics.ListAPIView):
-    serializer_class = SpeciesSerializer    
+    serializer_class = SpeciesSerializer  
 
+    @staticmethod
+    def get_alt_fieldname(field):
+        if field in field_mappings:
+            field = field_mappings[field]
+            return field
+        return field
+    
     def get_queryset(self):
         queryset = Species.objects.all()
-        lu_field = self.kwargs.get('lu_field')
-        lu_value = self.kwargs.get('lu_value')
-        
-        if lu_field and lu_value:
-            if lu_field in field_mappings:
-                lu_field = field_mappings[lu_field]
-                filter_kwargs = {f"{lu_field}__iexact": lu_value}
-                queryset = queryset.filter(**filter_kwargs)
-            else:
-                filter_kwargs = {f"{lu_field}__iexact": lu_value}
-                queryset = queryset.filter(**filter_kwargs)
-
         return queryset
     
-    # Since ListAPIView returns a list, return a modified list instead
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
+    @staticmethod
+    def filter_queryset(queryset, filters, condition):
+        queries = [Q(**{f"{key}__iexact": value}) for key, value in filters.items()]
 
-        return Response({
-            "results": len(serializer.data),
-            "data": serializer.data
-        })
+        queryset = \
+            queryset.filter( \
+            reduce( \
+                operator.or_ if condition=='or' else operator.and_, \
+                queries
+        ))
+        
+        return queryset
+        
+
+    def get(self, request, filters=None):
+        queryset = self.get_queryset()
+        filter_dict = {} 
+
+        if filters:
+            filter_pairs = filters.strip("/").split("/")
+            for pair in filter_pairs:
+                key, value = pair.split(":", 1)
+                key = self.get_alt_fieldname(key)
+                filter_dict[key] = value
+            
+            
+            queryset = self.filter_queryset(queryset=queryset, filters=filter_dict, condition='and')
+            serializer = self.get_serializer(queryset, many=True)
+            
+            return Response({
+                "results": len(serializer.data),
+                "data": serializer.data
+            })
